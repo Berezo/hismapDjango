68c844ad46628f6a3cff7fade96069f4c022b75d
Filip Berezowski <filipo1998.98@o2.pl>
2022-11-06 12:01:13 +0100

    HistoricalEvent Model

--- hismapapp/models.py
+++ hismapapp/models.py
@@ -1,3 +1,23 @@
 from django.db import models
 
-# Create your models here.
+class HistoricalEvent(models.Model):
+    class Meta:
+        db_table = 'historical_event'
+        ordering = ['data']
+    
+    ogc_fid = models.AutoField(primary_key=True, blank=True)
+    name = models.CharField(blank=True, null=True, max_length=80)
+    date = models.DateField(blank=True, null=True)
+    description = models.CharField(blank=True, null=True, max_length=254)
+    belligerent_1 = models.CharField(blank=True, null=True, max_length=254)
+    belligerent_2 = models.CharField(blank=True, null=True, max_length=254)
+    commander_1 = models.CharField(blank=True, null=True, max_length=254)
+    commander_2 = models.CharField(blank=True, null=True, max_length=254)
+    strength_1 = models.CharField(blank=True, null=True, max_length=254)
+    strength_2 = models.CharField(blank=True, null=True, max_length=254)
+    result = models.CharField(blank=True, null=True, max_length=254)
+    historical_context = models.CharField(blank=True, null=True, max_length=254)
+    geometry = models.PointField(srid=3857, blank=True, null=True)
+    
+    def __str__(self):
+        return f'{self.name}'
